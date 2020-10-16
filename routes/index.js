9f434bdc7c586b69521338cde10e6b034f78b489
du <new666@qq.com>
2020-10-16 18:00:00 +0800

    登录和注册的功能开发

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,6 @@ router.get('/register',controllerIndex.register);
 //访问 admin 页面
 router.get('/admin',controllerIndex.admin);
 
-// 匹配补上的路由，跳转到 login 页面
-router.get('*',(req,res,next)=>{
-  res.redirect('/login');
-});
-
 // MVC ： Model (数据)   View(视图)    Controller(控制器)
 //    Controller(控制器) : 中间层去连接 M 和 V。
 
